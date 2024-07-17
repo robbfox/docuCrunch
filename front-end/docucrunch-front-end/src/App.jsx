fe0b2c705905e67c314550aaf096ab1939a266f1
“selam” <Selamkd@users.noreply.github.com>
2024-07-17 14:54:52 +0100

    add tailwind config

--- front-end/docucrunch-front-end/src/App.jsx
+++ front-end/docucrunch-front-end/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Header from './components/Header.jsx';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello from the Dev team!!</h1>
+      <h1 className="bg-white">Hello!</h1>
     </>
   );
 }
--- front-end/docucrunch-front-end/src/components/Header.jsx
+++ front-end/docucrunch-front-end/src/components/Header.jsx
@@ -1,6 +1,13 @@
-function Header(){
-    <>
-        <div className={"flex justify-start w-100 fixed top-0"}>
-        </div>
-    </>
+import crunchLogo from '../assets/docuCrunchLogo.png';
+
+function Header() {
+  <>
+    <div className="flex justify-start w-100 fixed top-0 bg-pink">
+      <div className="flex justify-start w-100">
+        <img src={crunchLogo} className="h-8" alt="react logo" />
+      </div>
+    </div>
+  </>;
 }
+
+export default Header;
