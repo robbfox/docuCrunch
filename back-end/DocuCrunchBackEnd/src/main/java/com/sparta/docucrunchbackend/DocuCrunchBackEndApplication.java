05190db05ad48a5a75e6b77306aeeb126bb1a044
“selam” <Selamkd@users.noreply.github.com>
2024-07-17 10:51:54 +0100

    create spring boot project and add gitignore properties

---
+++ back-end/DocuCrunchBackEnd/src/main/java/com/sparta/docucrunchbackend/DocuCrunchBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.docucrunchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuCrunchBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocuCrunchBackEndApplication.class, args);
+    }
+
+}
---
+++ back-end/DocuCrunchBackEnd/src/test/java/com/sparta/docucrunchbackend/DocuCrunchBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.docucrunchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuCrunchBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
