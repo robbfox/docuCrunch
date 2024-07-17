584e90d2b1b3533eaf8fb32c774d102d977d32f8
Kar-ho Lee <k-h_lee@hotmail.co.uk>
2024-07-17 13:20:34 +0100

    web automation template complete

---
+++ webAutomationTests/src/test/java/webTestFramework/stepDefs/StepDefHooks.java
@@ -0,0 +1,37 @@
+package webTestFramework.stepDefs;
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class StepDefHooks extends StepDefsSuper {
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(DRIVER_LOCATION))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+    @Before
+    public void setup() {
+        webDriver = new RemoteWebDriver(service.getUrl(), getChromeOptions());
+    }
+
+    @After
+    public void afterEach() {
+        webDriver.quit();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        service.stop();
+    }
+}
