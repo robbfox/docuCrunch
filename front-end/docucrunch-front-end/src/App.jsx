6ab3e04e5690eedef99ecb99e6a12405b5fbd3a2
RayFFH <phoenix222smart@gmail.com>
2024-07-23 11:26:58 +0100

    renaming components to temp

--- front-end/docucrunch-front-end/src/App.jsx
+++ front-end/docucrunch-front-end/src/App.jsx
@@ -2,10 +2,10 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import Home from '../src/Components/index.jsx';
-import Layout from '../src/Components/Layout/index.jsx';
+import Home from './temp/index.jsx';
+import Layout from './temp/Layout/index.jsx';
 import { Router, Routes, Route } from 'react-router-dom';
-import Summarise from '../src/Components/summarise.jsx';
+import Summarise from './temp/summarise.jsx';
 import './styles/App.css';
 
 function App() {
